10ce602c367faa5ca993e0c2dd977235ab556818
CosimoTramonte <cosimotramonte7@gmail.com>
2023-03-22 11:15:47 +0100

    commit after lessons

--- js/script.js
+++ js/script.js
@@ -1,68 +1,63 @@
 //varabili
 const name = document.getElementById("name");
-
+const kilometres = document.getElementById("kilometres");
 const age = document.getElementById("ageselect");
-const btnGenera = document.getElementById("btnGenera");
-const btnAnnulla = document.getElementById("btnAnnulla");
-
-console.log(name, kilometres, age, btnGenera, btnAnnulla);
-
-//price calculation
-const pricePerKilometre = 0.21;
-
-
-const firstPercentage = 0.2;
-const secondPercentage = 0.4;
-
-
-
-let finalPrize = 0;
 
 let ageselect = document.getElementById("ageselect");
 
+
+const btnGenera = document.getElementById("btnGenera");
+
+let validForm = true;
+
+let finalPrize = 0;
+
 btnGenera.addEventListener("click", function(){
-    const kilometres = document.getElementById("kilometres").value;
-    let smallDiscount = (kilometres * pricePerKilometre * firstPercentage);
-    let bigDiscount = (kilometres * pricePerKilometre * secondPercentage);
-    let prize = kilometres * pricePerKilometre;
-    document.getElementById("outputName").innerHTML = name.value;
-    name.value = "";
 
-   
+    document.getElementById("outputName").innerHTML = name.value
     
-   
 
-    if(document.getElementById("ageselect").selectedindex == 2){
+    const pricePerKilometre = 0.21;
+    const firstPercentage = 0.2;
+    const secondPercentage = 0.4;
+    
+    
+    let prize = kilometres.value * pricePerKilometre;
+    let smallDiscount = (prize * firstPercentage);
+    let bigDiscount = (prize * secondPercentage);
+
+    console.log(smallDiscount)
+    
+    if(document.getElementById("ageselect").value === "scontoSilverTicket"){
         finalPrize = prize - smallDiscount
-    }else if(document.getElementById("ageselect").selectedindex == 3){
+    }else if(document.getElementById("ageselect").value === "scontoGoldTicket"){
         finalPrize = prize - bigDiscount
     }else{
         finalPrize = prize
     }
 
-    console.log("finalprice",finalPrize, );
-    document.getElementById("outputFinalPrize").innerHTML = finalPrize
-
-   
     document.getElementById("outputAgeSelect").innerHTML = age.value;
+
+
+    document.getElementById("outputFinalPrize").innerHTML = `${finalPrize} $`
+
+    const carrozza = Math.ceil(Math.random() * 10)
+    const codiceCP = Math.floor(Math.random() * (99000 - 10000 + 1) + 99000)
+
+    document.getElementById("output1").innerHTML = carrozza
+    document.getElementById("output2").innerHTML = codiceCP
+
+    document.querySelector("card").classList.remove("hide")
+
+
+})
+
+ const btnAnnulla = document.getElementById("btnAnnulla")
+
+ btnAnnulla.addEventListener("click", function(){
     age.value = "";
     kilometres.value = "";
+    name.value = "";
 
-})
-
-const maintext = document.querySelector('.main-text');
-
-btnAnnulla.addEventListener("click", function(){
-
-    if(maintext.classList.contains('hide')){
-        maintext.classList.remove('hide');
-      }else{
-        maintext.classList.add('hide');
-    }
-})
-
-
-
-
-
-
+    document.querySelector("card").classList.add("hide")
+ })
